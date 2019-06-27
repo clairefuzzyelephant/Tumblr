1e45c1fc4877ca867284726fdd870e3a7f019c05
Claire Cheng <clairec@fb.com>
2019-06-27 10:58:03 -0700

    photocell

---
+++ Tumblr/PhotoCell.h
@@ -0,0 +1,17 @@
+//
+//  PhotoCell.h
+//  Tumblr
+//
+//  Created by clairec on 6/27/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Tumblr/PhotosViewController.m
+++ Tumblr/PhotosViewController.m
@@ -8,7 +8,12 @@
 
 #import "PhotosViewController.h"
 
-@interface PhotosViewController ()
+@interface PhotosViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (strong, nonatomic) NSArray *posts;
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
 
 @end
 
@@ -16,8 +21,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tableView.dataSource=self;
+    self.tableView.delegate= self;
     // Do any additional setup after loading the view.
+   
+   
+   NSURL *url = [NSURL URLWithString:@"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV"];
+   NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+   NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+   NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+      if (error != nil) {
+         NSLog(@"%@", [error localizedDescription]);
+      }
+      else {
+         NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+         
+         //NSLog(@"%@", dataDictionary);
+         // TODO: Get the posts and store in posts property
+         
+         NSDictionary *responseDictionary = dataDictionary[@"response"];
+         self.posts = responseDictionary[@"posts"];
+         // TODO: Reload the table view
+      }
+   }];
+   [task resume];
 }
+
 
 /*
 #pragma mark - Navigation
@@ -28,5 +57,16 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    cell.textLabel.text = [NSString stringWithFormat: @"This is row %ld", (long)indexPath.row];
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
 
 @end
